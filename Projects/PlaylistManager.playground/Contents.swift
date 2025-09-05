3f30d7d18d43074cf23125d0750074d5ea0420db
Andrew Vander Wilt <andyvanderwilt@gmail.com>
2025-09-05 11:55:27 -0600

    9/5/25 classwork, finished playlist manager project

--- Projects/PlaylistManager.playground/Contents.swift
+++ Projects/PlaylistManager.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-struct Song {
+struct Song: Equatable {
     let name: String
     let artist: String
     let duration: Int
@@ -36,13 +36,10 @@ class Playlist {
     // Querying / reading
     var count: Int { get { songs.count } }
     
-    func allSongs() -> [Song] {
-        var allSongsArray: [Song] = []
+    func allSongs(){
         for song in songs {
-            allSongsArray.append(song)
+            print("\(song.name), \(song.artist): \(song.duration) minutes")
         }
-        print(allSongsArray)
-        return allSongsArray
     }
     
     func totalDuration() -> Int {
@@ -50,13 +47,13 @@ class Playlist {
         for song in songs {
             totalDuration += song.duration
         }
-        print(totalDuration)
+        print("Total Durantion: \(totalDuration) minutes.")
         return totalDuration
     }
     
     func currentSong() -> Song? {
         if currentlyPlaying != nil {
-            print(songs[currentlyPlaying!])
+            print("Currently playing \(songs[currentlyPlaying!].name) by \(songs[currentlyPlaying!].artist)")
             return songs[currentlyPlaying!]
         } else {
             return nil
@@ -66,14 +63,14 @@ class Playlist {
     // Playback navigation
     func play(at index: Int) -> Song? {
         currentlyPlaying = index
-        print(index)
+        print("Now playing \(songs[index].name) by \(songs[index].artist)")
         return songs[index]
     }
     
     func playNext() -> Song? {
         if currentlyPlaying != nil {
             currentlyPlaying! += 1
-            print(songs[currentlyPlaying!])
+            print("Now playing \(songs[currentlyPlaying!].name) by \(songs[currentlyPlaying!].artist)")
             return songs[currentlyPlaying!]
         } else {
             return nil
@@ -83,7 +80,7 @@ class Playlist {
     func playPrevious() -> Song? {
         if currentlyPlaying != nil {
             currentlyPlaying! -= 1
-            print(songs[currentlyPlaying!])
+            print("Now playing \(songs[currentlyPlaying!].name) by \(songs[currentlyPlaying!].artist)")
             return songs[currentlyPlaying!]
         } else {
             return nil
@@ -94,20 +91,46 @@ class Playlist {
     func shuffle() {
         songs.shuffle()
     }
+    
+    func sortByName() {
+        songs = songs.sorted { $0.name < $1.name }
+    }
+    
+    func sortByDuration() {
+        songs = songs.sorted { $0.duration < $1.duration }
+    }
+    
+    func sortByArtist() {
+        songs = songs.sorted { $0.artist < $1.artist }
+    }
+    
+    func moveSong(song: Song, to index: Int) {
+        if let i = songs.firstIndex(of: song) {
+            songs.remove(at: i)
+        }
+        songs.insert(song, at: index)
+    }
 }
 
-var song1 = Song(name: "Crocadile Tearz", artist: "J. Cole", duration: 4)
-var song2 = Song(name: "Sugarcat", artist: "Dope Lemon", duration: 3)
-var song3 = Song(name: "Marinade", artist: "Dope Lemon", duration: 4)
-var song4 = Song(name: "Law & Order", artist: "BigXthaPlug", duration: 3)
-var song5 = Song(name: "London Roads", artist: "Lil Wayne", duration: 4)
+var s1 = Song(name: "Crocadile Tearz", artist: "J. Cole", duration: 4)
+var s2 = Song(name: "Sugarcat", artist: "Dope Lemon", duration: 3)
+var s3 = Song(name: "Marinade", artist: "Dope Lemon", duration: 4)
+var s4 = Song(name: "Law & Order", artist: "BigXthaPlug", duration: 3)
+var s5 = Song(name: "London Roads", artist: "Lil Wayne", duration: 4)
 
 var myPlaylist = Playlist(name: "Big steppas playlist", author: "Andrew", songs: [], currentlyPlaying: nil)
 
-myPlaylist.songs.append(song1)
-myPlaylist.songs.append(song2)
-myPlaylist.songs.append(song3)
-myPlaylist.songs.append(song4)
-myPlaylist.songs.append(song5)
+myPlaylist.songs.append(s1)
+myPlaylist.songs.append(s2)
+myPlaylist.songs.append(s3)
+myPlaylist.songs.append(s4)
+myPlaylist.songs.append(s5)
 
 myPlaylist.allSongs()
+print("--")
+myPlaylist.moveSong(song: s2, to: 4)
+myPlaylist.allSongs()
+print("--")
+myPlaylist.play(at: 1)
+myPlaylist.currentSong()
+myPlaylist.totalDuration()
