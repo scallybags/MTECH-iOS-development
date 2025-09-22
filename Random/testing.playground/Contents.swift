8eee178af1df4f8a3a09ea8af53a5dd2ba6830c9
Andrew Vander Wilt <andyvanderwilt@gmail.com>
2025-09-22 13:00:50 -0600

    Finished Closures & Startedextensions

--- Random/testing.playground/Contents.swift
+++ Random/testing.playground/Contents.swift
@@ -1,24 +1,38 @@
 import UIKit
 
-enum Move {
-    case rock, paper, scissors
+//enum Move {
+//    case rock, paper, scissors
+//}
+//
+//enum Outcome {
+//    case tie, player1Wins, player2Wins
+//}
+//
+//func rockPaperScissors(player1: Move, player2: Move) -> Outcome {
+//    guard player1 != player2 else { return .tie }
+//    switch player1 {
+//    case .rock:
+//        return player2 == .paper ? .player2Wins : .player1Wins
+//    case .paper:
+//        return player2 == .scissors ? .player2Wins : .player1Wins
+//    case .scissors:
+//        return player2 == .rock ? .player2Wins : .player1Wins
+//    }
+//}
+//let result = rockPaperScissors(player1: .paper, player2: .rock)
+//print(result)
+
+let names = ["Zach", "Parker", "Joe", "Andy"]
+
+let counts = names.map { $0.count }
+
+let closure = { (numbers: [Int]) -> Int in
+        return 0
 }
 
-enum Outcome {
-    case tie, player1Wins, player2Wins
-}
+//let sortedTracks = tracks.sorted { (firstTrack: Track, secondTrack: Track) -> Bool in
+//    return firstTrack.trackNumber < secondTrack.trackNumber
+//}
 
-func rockPaperScissors(player1: Move, player2: Move) -> Outcome {
-    guard player1 != player2 else { return .tie }
-    switch player1 {
-    case .rock:
-        return player2 == .paper ? .player2Wins : .player1Wins
-    case .paper:
-        return player2 == .scissors ? .player2Wins : .player1Wins
-    case .scissors:
-        return player2 == .rock ? .player2Wins : .player1Wins
-    }
-}
-let result = rockPaperScissors(player1: .paper, player2: .rock)
-print(result)
-
+let a = [1,2,3,4,5]
+let b = a.sorted()
---
+++ Textbook Files/Data collections/2 - Working With the Web/2 - Extensions/Lab - Extensions.playground/Pages/1. Exercise - Extensions.xcplaygroundpage/Contents.swift
@@ -0,0 +1,40 @@
+/*:
+## Exercise - Extensions
+ 
+ Define an extension to `Character` that includes a function `isVowel()`. The function returns `true` if the character is a vowel (a,e,i,o,u), and `false` otherwise. Be sure to properly handle uppercase and lowercase characters.
+ */
+//extension Character {
+//    func isVowel() -> Bool {
+//        self == Character("a") || Character("e") || Character("i") || Character("o") || Character("u")
+//    }
+//}
+
+//:  Create two `Character` constants, `myVowel` and `myConsonant`, and set them to a vowel and a consonant, respectively. Use the `isVowel()` methods on each constant to determine whether or not it's a vowel.
+
+
+//:  Create a `Rectangle` struct with two variable properties, `length` and `width`, both of type `Double`. Below the definition, write an extension to `Rectangle` that includes a function, `halved()`. This function returns a new `Rectangle` instance with half the length and half the width of the original rectangle.
+struct Rect {
+    var length: Double
+    var width: Double
+}
+
+extension Rect {
+    func halved() -> Rect {
+        Rect(length: (self.length / 2) , width: (self.width / 2))
+    }
+    
+    mutating func half() {
+        self = self.halved()
+    }
+}
+/*:
+ Within the existing `Rectangle` extension, add a new mutating function, `half()`, which updates the original rectangle to have half the length and half the width. Use the `halved()` function as part of the implementation for `half()`.
+ 
+ Below, create a variable `Rectangle` called `myRectangle`, and set its length to 10 and its width to 5. Create a second instance, `mySmallerRectangle`, that's the result of calling `halved()` on `myRectangle`. Then update the values of `myRectangle` by calling `half()` on itself. Print each of the instances.
+ */
+var myRectangle = Rect(length: 20.0, width: 10.0)
+var mySmallRect = myRectangle.halved()
+myRectangle.half()
+/*:
+page 1 of 2  |  [Next: App Exercise - Workout Extensions](@next)
+ */
