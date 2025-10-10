d527c69f1357ac9ff6ab9947dee4bebe42b4f900
Andrew Vander Wilt <andyvanderwilt@gmail.com>
2025-10-09 21:25:14 -0600

    Updated Names and Ages of a fam project

--- Projects/MeetMyFamily/MeetMyFamily/MembersList.swift
+++ Projects/MeetMyFamily/MeetMyFamily/MembersList.swift
@@ -7,9 +7,13 @@
 
 
 let members: [Member] = [
+    Member(name: "Quen", age: 22),
+    Member(name: "Aly", age: 22),
     Member(name: "Andrew", age: 24),
-    Member(name: "Andrew", age: 24),
-    Member(name: "Andrew", age: 24),
-    Member(name: "Andrew", age: 24),
-    Member(name: "Andrew", age: 24),
+    Member(name: "Eric", age: 27),
+    Member(name: "Emily", age: 29),
+    Member(name: "Sarah", age: 30),
+    Member(name: "Peter", age: 32),
+    Member(name: "Kit", age: 32),
+    Member(name: "Sandra", age: 56),
 ]
